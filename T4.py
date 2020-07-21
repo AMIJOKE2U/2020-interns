df94f77167c60afd5a8dc2f7118bf275c7d002ff
AMIJOKE2U <saiteja.murari2000@gmail.com>
2020-07-21 19:01:11 +0530

    T4 is completed

---
+++ T4.py
@@ -0,0 +1,46 @@
+import requests
+import json
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+new_s_date='2019-01-01'
+new_e_date='2019-01-31'
+
+
+url1='https://api.exchangeratesapi.io/history?start_at={}&end_at={}'.format(new_s_date,new_e_date)
+response=requests.get(url1)
+file=response.text
+data=json.loads(file)
+
+json_obj=json.dumps(data, indent=4)
+with open('data.json','w') as outfile:
+    outfile.write(json_obj)
+
+
+    
+fetch_rate1=dict()
+fetch_rate2=dict()
+
+for i in data['rates']:
+    date=datetime.strptime(i,'%Y-%m-%d').date()
+    if(date.month==1 and date.year==2019):
+        fetch_rate1[date.day]=data['rates'][str(i)]['INR']
+        fetch_rate2[date.day]=data['rates'][str(i)]['GBP']
+
+INR_plot=sorted(fetch_rate1.items())
+GBP_plot=sorted(fetch_rate2.items())
+
+dates,rates1=zip(*INR_plot)
+dates,rates2=zip(*GBP_plot)
+
+plt.figure(figsize=(20,20))
+plt.plot(dates,rates1,label='Current INR Rate-'+str(max(rates1)))
+plt.plot(dates,rates2,label='Current GBP Rate-'+str(max(rates2)))
+
+plt.xlabel('Dates of Jan 2019')
+plt.ylabel('Rates of Jan 2019')
+plt.xticks(np.arange(32),rotation=90)
+plt.title('Exchange rate of INR and GBP against EUR')
+plt.legend()
+plt.show()
